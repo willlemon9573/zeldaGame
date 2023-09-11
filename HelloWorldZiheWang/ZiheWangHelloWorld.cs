59176e61570309806d300f0aad4ade4f5ee65ad7
Zihe Wang <zihe030716@outlook.com>
2023-09-11 18:31:51 -0400

    copyPaste HelloWorld and create a copy file named HelloWorldZiheWang

---
+++ HelloWorldZiheWang/ZiheWangHelloWorld.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorldZiheWang
+{
+    public class ZiheWangHelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); /* initial code */
+            /* TODO - Branch off this branch and add your own hello world */
+        }
+    }
+}
