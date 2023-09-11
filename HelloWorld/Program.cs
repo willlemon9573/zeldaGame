7cfd71e5e79ed4c6ed15658a9b033c955e586df7
Aren <82329186+heishmanA@users.noreply.github.com>
2023-09-11 18:15:44 -0400

    Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); /* initial code */
+            /* TODO - Branch off this branch and add your own hello world */
+        }
+    }
+}
