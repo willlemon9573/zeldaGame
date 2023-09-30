ada89c992cb71adc1d04dacc88c2678e5bf2cd31
Zihe Wang <zihe030716@outlook.com>
2023-09-30 02:18:17 -0400

    not finish

---
+++ Commands/betterArrowWeapon.cs
@@ -0,0 +1,30 @@
+﻿using SprintZero1.Factories;
+using SprintZero1.Sprites;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace SprintZero1.Commands
+{
+    public class betterArrowWeapon : ICommand
+    {
+        private Game1 game;
+        private int Direction;
+        private Vector2 location;
+        private WeaponSpriteFactory WeaponFactory;
+        ISprite newSprite;
+
+        public betterArrowWeapon(Game1 game)
+        {
+            this.game = game;
+            this.WeaponFactory = WeaponSpriteFactory.Instance;
+        }
+
+        public void Execute()
+        {
+            location = game.position;
+            Direction = game.CurrentDirection;
+            newSprite = WeaponFactory.CreateArrowSprite("better", location, 3, Direction);
+            game.Weapon = newSprite;
+        }
+
+    }
+}
