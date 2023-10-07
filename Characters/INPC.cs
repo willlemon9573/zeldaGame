d4974993aadfcaee2aae6e508e3609691b57c13d
Aren <82329186+heishmanA@users.noreply.github.com>
2023-10-07 00:53:23 -0400

    Cleaned up code

--- Characters/INPC.cs
+++ Characters/INPC.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SprintZero1.Characters
+﻿namespace SprintZero1.Characters
 {
     internal interface INPC
     {
--- Characters/Projectile.cs
+++ Characters/Projectile.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SprintZero1.Characters
 {
@@ -13,9 +8,9 @@ namespace SprintZero1.Characters
         public Vector2 pos;
         SpriteBatch spriteBatch;
         Rectangle source = new Rectangle(238, 154, 8, 16);
-        Rectangle destination = new Rectangle(0,0,20,40);
+        Rectangle destination = new Rectangle(0, 0, 20, 40);
         Texture2D texture;
-        
+
 
         public Projectile(SpriteBatch spriteBatch, Vector2 startPos, Texture2D texture)
         {
@@ -28,8 +23,8 @@ namespace SprintZero1.Characters
         public void Update(GameTime timer)
         {
             pos.X -= 6;
-            destination.X = (int) pos.X;
-            destination.Y = (int) pos.Y;    
+            destination.X = (int)pos.X;
+            destination.Y = (int)pos.Y;
         }
 
         public void Draw()
@@ -37,6 +32,6 @@ namespace SprintZero1.Characters
             spriteBatch.Draw(texture, destination, source, Color.White);
         }
 
-        
+
     }
 }
