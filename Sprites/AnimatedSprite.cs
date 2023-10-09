f55cfc96cdbce34699b50c3c873f6fbe00f9e409
Aren <82329186+heishmanA@users.noreply.github.com>
2023-10-08 22:10:16 -0400

    Fixed sprites

--- Sprites/AnimatedSprite.cs
+++ Sprites/AnimatedSprite.cs
@@ -80,14 +80,14 @@ namespace SprintZero1.Sprites
         {
             _currentFrame = 0;
         }
-        public void Draw(SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects = SpriteEffects.None, float scale = 1f, float rotation = 0f)
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects = SpriteEffects.None, float rotation = 0f)
         {
             /* Build the source rectangle and destination rectangle to draw onto screen */
             Rectangle sourceRectangle = _sourceRectangles[_currentFrame];
             int height = sourceRectangle.Height;
             int width = sourceRectangle.Width;
             Vector2 origin = new Vector2(width / 2, height / 2); /* origin of the drawing in the middle for rotation */
-            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, (int)(width * scale), (int)(height * scale));
+            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, (int)(width), (int)(height));
             spriteBatch.Draw(_spriteSheet, destinationRectangle, sourceRectangle, Color.White, rotation, origin, spriteEffects, 0f);
         }
 
--- Sprites/ISprite.cs
+++ Sprites/ISprite.cs
@@ -13,7 +13,7 @@ namespace SprintZero1.Sprites
         /// <param name="spriteEffects">Horizontal or vertical rotation(Optional)</param>
         /// <param name="rotation">The angle in radians to rate the _sprite around the origin(Optional)</param>
         /// <param name="scale">The uniform multiple to WINDOW_SCALE the _sprite width and height(Optional)</param>
-        public void Draw(SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects = SpriteEffects.None, float scale = 1f, float rotation = 0f);
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects = SpriteEffects.None, float rotation = 0f);
 
         public void Update(GameTime gameTime);
     }
