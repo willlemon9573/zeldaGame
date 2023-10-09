237fcbe58861e68df4631dcafc1acc32d1bfb08e
Aren <82329186+heishmanA@users.noreply.github.com>
2023-10-08 22:14:49 -0400

    Added some documentation

--- Entities/IEntity.cs
+++ Entities/IEntity.cs
@@ -5,9 +5,19 @@ namespace SprintZero1.Entities
 {
     internal interface IEntity
     {
+        /// <summary>
+        /// Set and Get the Entity Position
+        /// </summary>
         public Vector2 Position { get; set; }
+        /// <summary>
+        /// Update the Entity 
+        /// </summary>
+        /// <param name="gameTime">Total Game Time</param>
         public void Update(GameTime gameTime);
-
+        /// <summary>
+        /// Draw the Entity
+        /// </summary>
+        /// <param name="spriteBatch"></param>
         public void Draw(SpriteBatch spriteBatch);
     }
 }
