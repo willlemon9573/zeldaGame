b0d8fd1de264b14eb53cbf217f0e9c1de83242a9
willlemon9573 <willclemon@gmail.com>
2023-10-09 15:47:02 -0400

    xmlReader

---
+++ XMLFiles/XMLParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SprintZero1.XMLFiles
+{
+    public class XMLParser
+    {
+        public XMLParser(Game1 game)
+        { 
+        
+            
+
+        }
+
+        public void Parse(String fileName) 
+        {
+            XmlReader xmlReader = new XmlTextReader(fileName);
+            while (xmlReader.Read()) 
+            {
+                String nodeType = xmlReader.NodeType.ToString();
+                switch (nodeType)
+                {
+                    case "Blocks":
+
+                        break;
+                    case "Items":
+
+                        break;
+                    case "Enemies":
+
+                        break;
+                }
+            }
+
+          
+        }
+
+
+    }
+}
