6f81ad8c285dc4cf5148094f197790104833fe27
Aren <82329186+heishmanA@users.noreply.github.com>
2023-10-14 00:47:52 -0400

    Completed remade how movement is handled

--- Controllers/KeyboardController.cs
+++ Controllers/KeyboardController.cs
@@ -2,19 +2,77 @@ using Microsoft.Xna.Framework.Input;
 using SprintZero1.Commands;
 using SprintZero1.Entities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SprintZero1.Controllers
 {
     internal class KeyboardController : IController
     {
         private readonly Dictionary<Keys, ICommand> keyboardMap;
-        private Keys prevKey;
+        List<Keys> movementKeys = new List<Keys>() { Keys.Up, Keys.Down, Keys.Left, Keys.Right, Keys.W, Keys.S, Keys.A, Keys.D };
+        List<Keys> previouslyPressedKeys = new List<Keys>();
+        Stack<Keys> priorityKeyStack = new Stack<Keys>();
         /// <summary>
         /// Construct an object to control the keyboard
         /// </summary>
         public KeyboardController()
         {
             keyboardMap = new Dictionary<Keys, ICommand>();
+        }
+        /// <summary>
+        /// Removes any key not being pressed from the priority key stack
+        /// </summary>
+        private void flipAndClean(KeyboardState currentKeyboardState)
+        {
+            if (priorityKeyStack.Count > 0)
+            {
+                Keys k = priorityKeyStack.Pop();
+                flipAndClean(currentKeyboardState);
+                if (Keyboard.GetState().IsKeyDown(k))
+                {
+                    priorityKeyStack.Push(k);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A function to allow multiple movement keys to be pressed at once
+        /// </summary>
+        /// <param name="movementKey">the current movement key that needs to be checked</param>
+        /// <param name="currentKeyboardState">the current stay of the keyboard</param>
+        void handleMovementInput(Keys movementKey, KeyboardState currentKeyboardState)
+        {
+            /* Add key to stack if it's not in the stack already */
+            if (!priorityKeyStack.Contains(movementKey))
+            {
+                priorityKeyStack.Push(movementKey);
+                /* don't execute key because it will push through colliders 
+                 * if the key is pressed more than once */
+                return;
+            }
+            /* Key is in stack already, check if this key is the movement command that the
+             * player wants to execute */
+            while (priorityKeyStack.Count > 0)
+            {
+                Keys priorityKey = priorityKeyStack.Pop();
+                /* execute priority key only when the key being checked matches. */
+                if (priorityKey == movementKey)
+                {
+                    priorityKeyStack.Push(priorityKey);
+                    keyboardMap[priorityKey].Execute();
+                    break;
+                }
+                else if (currentKeyboardState.IsKeyDown(priorityKey))
+                {
+                    /* clean up priorty key stack as the current priority key was let go
+                     * then push the priority key back onto the stack */
+                    flipAndClean(currentKeyboardState);
+                    priorityKeyStack.Push(priorityKey);
+
+                    break;
+                }
+            }
+
         }
 
         public void LoadDefaultCommands(Game1 game, IEntity playerEntity)
@@ -29,7 +87,7 @@ namespace SprintZero1.Controllers
             keyboardMap.Add(Keys.A, new MoveLeftCommand(playerEntity));
             keyboardMap.Add(Keys.D, new MoveRightCommand(playerEntity));
             /* Attack Commands */
-            keyboardMap.Add(Keys.Z, new LinkAttackCommand(game));
+            keyboardMap.Add(Keys.Z, new LinkAttackCommand(playerEntity));
             /* Other commands */
             keyboardMap.Add(Keys.D0, new ExitCommand(game));
             keyboardMap.Add(Keys.U, new PreviousItemCommand(game));
@@ -42,21 +100,25 @@ namespace SprintZero1.Controllers
 
         public void Update()
         {
-            // monogame updates too quickly for this
-            Keys[] pressedkeys = Keyboard.GetState().GetPressedKeys();
+            /* handling movement? */
+            KeyboardState currentKeyboardState = Keyboard.GetState();
+            Keys[] pressedKeys = currentKeyboardState.GetPressedKeys();
 
-            foreach (Keys key in pressedkeys)
+
+            foreach (Keys key in pressedKeys)
             {
-                if (prevKey == key && keyboardMap.ContainsKey(key))
+                Keys nextKey = key;
+                if (movementKeys.Contains(nextKey))
                 {
-                    keyboardMap[key].Execute();
+                    handleMovementInput(nextKey, currentKeyboardState);
                 }
             }
-            /* Temporary fix to make sure player doesn't move at an angle */
-            if (pressedkeys.Length > 0 && prevKey != pressedkeys[0])
+            /* clean up priority key stack if no buttons are pressed */
+            if (pressedKeys.Length == 0 && priorityKeyStack.Count > 0)
             {
-                prevKey = pressedkeys[0];
+                priorityKeyStack.Clear();
             }
+            previouslyPressedKeys = pressedKeys.ToList<Keys>();
         }
     }
 }
