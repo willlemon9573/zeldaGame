d5385c46acbe096ec4cfbb99bc5072f4c0ac9c4c
Zihe Wang <zihe030716@outlook.com>
2023-11-12 01:19:46 -0500

    iterm selection keyboard

--- Controllers/KeyboardControllerForItemSelection.cs
+++ Controllers/KeyboardControllerForItemSelection.cs
@@ -1,125 +1,84 @@
-/*using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input;
+using SprintZero1.Commands.MenuCommandsFolder;
 using SprintZero1.Commands;
 using SprintZero1.Entities;
 using SprintZero1.Managers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using SprintZero1.GameStateMenu;
+
 namespace SprintZero1.Controllers
 {
-    public delegate void PausedStateUpdater(Game1 game);
-    internal class KeyboardControllerForItemSelection : IController
+    /// <summary>
+    /// The KeyboardControllerForItemSelection class is responsible for handling keyboard inputs
+    /// specifically for item selection in the game. It maps specific keys to corresponding commands
+    /// and executes these commands based on player input.
+    /// </summary>
+    /// <author>Zihe Wang</author>
+    internal class KeyboardControllerForItemSelection
     {
+        // Maps keyboard keys to ICommand objects
         private Dictionary<Keys, ICommand> _keyboardMap;
-        private readonly List<Keys> _movementKeyList;
+
+        // List of keys used for item selection
+        private readonly List<Keys> _ItemSelectionKeyList;
+
+        // List to track keys that were pressed in the previous frame
         private List<Keys> _previouslyPressedKeys;
-        private readonly Stack<Keys> _movementKeyStack;
+
         /// <summary>
-        /// Construct an object to control the keyboard
+        /// Constructor for KeyboardControllerForItemSelection. Initializes key lists.
         /// </summary>
         public KeyboardControllerForItemSelection()
         {
             _previouslyPressedKeys = new List<Keys>();
-            _movementKeyStack = new Stack<Keys>();
-            _movementKeyList = new List<Keys>() { Keys.Up, Keys.Down, Keys.Left, Keys.Right, Keys.W, Keys.S, Keys.A, Keys.D };
+            _ItemSelectionKeyList = new List<Keys>() { Keys.Left, Keys.Right, Keys.J };
         }
 
         /// <summary>
-        /// Recursively removes movement keys from the movement key stack that are not in the list of pressed keys
+        /// Loads the control mappings for a specific player.
         /// </summary>
-        /// <param name="pressedKeys">collection of currently pressed keys</param>
-        private void FlipAndClean(Keys[] pressedKeys)
+        /// <param name="player">The player entity for which controls are being set up.</param>
+        /// <param name="itemSelectionMenu">The item selection menu used in control mapping.</param>
+        public void LoadControls(IEntity player, ItemSelectionMenu itemSelectionMenu)
         {
-            if (_movementKeyStack.Count > 0)
-            {
-                Keys k = _movementKeyStack.Pop();
-                FlipAndClean(pressedKeys);
-                if (pressedKeys.Contains(k))
-                {
-                    _movementKeyStack.Push(k);
-                }
-            }
+            // Instantiate command objects for each key action
+            var getPreviousWeaponCommand = new GetPreviousWeaponCommand(itemSelectionMenu);
+            var getNextWeaponCommand = new GetNextWeaponCommand(itemSelectionMenu);
+            var setCurrentWeaponToPlayerCommand = new SetCurrentWeaponToPlayerCommand(player, itemSelectionMenu);
+
+            // Map keys to their respective commands
+            _keyboardMap[Keys.Left] = getPreviousWeaponCommand;
+            _keyboardMap[Keys.Right] = getNextWeaponCommand;
+            _keyboardMap[Keys.J] = setCurrentWeaponToPlayerCommand;
         }
 
         /// <summary>
-        /// A function to allow multiple movement keys to be pressed at once while maintaining the most recent key command is executed
+        /// Updates the state of the keyboard controller, executing commands based on key presses.
         /// </summary>
-        /// <param name="movementKey">the current movement key that needs to be checked</param>
-        void HandleMovementKey(Keys movementKey)
-        {
-            *//* 
-             * Add key to stack if it's not in the stack already and execute that key
-             * else execute the command of the movement key at the top of the stack
-             *//*
-            if (!_movementKeyStack.Contains(movementKey))
-            {
-                _movementKeyStack.Push(movementKey);
-            }
-            else if (movementKey == _movementKeyStack.Peek())
-            {
-                Keys keyRef = _movementKeyStack.Peek();
-                _keyboardMap[keyRef].Execute();
-            }
-        }
-        /// <summary>
-        /// Load the controls for the specific player.
-        /// </summary>
-        /// <param name="player">The player the controls will be loaded for</param>
-        public void LoadControls(IEntity player)
-        {
-            _keyboardMap = ControlsManager.GetKeyboardControls(player);
-        }
-
-        public void PausedStateUpdate(Game1 game)
-        {
-            KeyboardState currentKeyboardState = Keyboard.GetState();
-            Keys[] pressedKeys = currentKeyboardState.GetPressedKeys();
-
-            foreach (Keys key in pressedKeys)
-            {
-                if (key == Keys.Escape && !_previouslyPressedKeys.Contains(key))
-                {
-                    game.GameState.ChangeGameState(Enums.GameState.Playing);
-                }
-            }
-            _previouslyPressedKeys = pressedKeys.ToList();
-        }
-
         public void Update()
         {
             KeyboardState currentKeyboardState = Keyboard.GetState();
             Keys[] pressedKeys = currentKeyboardState.GetPressedKeys();
-            // no need to update if no keys have been pressed
-            if (pressedKeys.Length == 0 && _movementKeyStack.Count == 0 && _previouslyPressedKeys.Count == 0) { return; }
-            int totalKeyCount = 0;
-            *//* iterate over pressed key collection executing only valid keys in the keyboard map 
-               keeping track of the amount of movement keys that are also currently being pressed
-            *//*
+
+            // Return early if no keys are pressed and no keys were previously pressed
+            if (pressedKeys.Length == 0 && _previouslyPressedKeys.Count == 0)
+            {
+                return;
+            }
+
+            // Execute commands for newly pressed keys
             foreach (Keys key in pressedKeys)
             {
-                if (_movementKeyList.Contains(key))
-                {
-                    HandleMovementKey(key);
-                    totalKeyCount++;
-
-                }
-                else if (_keyboardMap.ContainsKey(key) && !_previouslyPressedKeys.Contains(key))
+                if (_keyboardMap.ContainsKey(key) && !_previouslyPressedKeys.Contains(key))
                 {
                     _keyboardMap[key].Execute();
                 }
             }
 
-            *//* Clean up movement key stack and/or set player to idle when 
-             * no keys are pressed. Checking both movement key count and previously pressed
-             * keys to prevent _idleCommand from executing more than once
-             *//*
-            if (totalKeyCount < _movementKeyStack.Count)
-            {
-                FlipAndClean(pressedKeys);
-            }
-
-            *//* store pressed keys as previously pressed keys *//*
+            // Update previously pressed keys
             _previouslyPressedKeys = pressedKeys.ToList<Keys>();
         }
     }
-}*/
+}
