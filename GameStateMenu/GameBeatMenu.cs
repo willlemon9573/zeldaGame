b403fa9ad8230bb49ef7d744adaf8ed709153af4
Zihe Wang <zihe030716@outlook.com>
2023-11-12 15:12:03 -0500

    game beat state menu finished

--- GameStateMenu/GameBeatMenu.cs
+++ GameStateMenu/GameBeatMenu.cs
@@ -3,39 +3,62 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace SprintZero1.GameStateMenu
 {
+    /// <summary>
+    /// Represents the post-game credits screen.
+    /// This class extends GameStateAbstract and manages the display of the flashing effect, merging animation,
+    /// and scrolling credits once the game is beaten.
+    /// </summary>
+    /// <author>Zihe Wang</author>
     public class GameBeatMenu : GameStateAbstract
     {
+        // Flags to control the state of the menu.
         private bool isFlashing = true;
         private bool isMerging = false;
         private bool hasMerged = false;
         private bool shouldDrawFlashing = false;
         private bool isWaiting = false;
 
+        // Timers to manage the timing of flashing, merging, and waiting periods.
         private float flashTimer = 0;
         private float mergeTimer = 0;
         private float waitTimer = 0;
+
+        // Position for the merge animation.
         private float mergePosition = 0;
 
+        // Counter for the number of flashes.
         private int flashCount = 0;
 
-        private const float flashMaxTime = 0.4f; // Time interval for flashing
-        private const int flashAmount = 4; // Number of flashes
-        private const float mergeFinishTime = 3.0f; // Time duration for merging animation
-        private const float waitTime = 2.0f; // Waiting time between flashing and merging
-        private const int RGB_BLACK = 225; // RGB value for white overlay color
-        private const int RGB_ALPHA = 60; // Alpha (transparency) value for white overlay
+        // Constants for timing configurations.
+        private const float flashMaxTime = 0.4f; // The interval of each flash.
+        private const int flashAmount = 4; // The total number of flashes.
+        private const float mergeFinishTime = 3.0f; // The duration of the merging animation.
+        private const float waitTime = 2.0f; // The wait time between flashing and merging.
 
+        // Constants for the overlay color.
+        private const int RGB_BLACK = 225; // The RGB value for the white overlay color.
+        private const int RGB_ALPHA = 60; // The alpha (transparency) value for the white overlay.
+
+        // Position for the scrolling credits text.
         private float scrollPosition;
+
+        // Text for the credits.
         private string creditsText = "Thank you \n for playing! \n Created by: EnterGame\n...\nTeamMember:\n Zihe Wang\n Aaron Heishman\n Muhammad Gheith\n Chih-Hsiang Jarek Tseng\n Will Lemon. ";
+
+        // Font for the credits text.
         private SpriteFont creditsFont;
+
+        // Flag to indicate if the credits have finished scrolling.
         private bool creditsRolled = false;
+
 
         public GameBeatMenu(Game1 game) : base(game)
         {
+            // Set the overlay color and load the font.
             Color WhiteOverlay = new Color(RGB_BLACK, RGB_BLACK, RGB_BLACK, RGB_ALPHA);
             creditsFont = game.Content.Load<SpriteFont>("PauseSetting");
             scrollPosition = HEIGHT;
-            _overlay.SetData(new[] { WhiteOverlay }); 
+            _overlay.SetData(new[] { WhiteOverlay });
         }
 
         public override void Update(GameTime gameTime)
--- GameStateMenu/PauseMenu.cs
+++ GameStateMenu/PauseMenu.cs
@@ -42,11 +42,11 @@ namespace SprintZero1.GameStateMenu
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_overlay, new Rectangle(0, 0, WIDTH, HEIGHT), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.02f);
+            spriteBatch.Draw(_overlay, new Rectangle(0, 0, WIDTH, HEIGHT), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.01f);
 
             Vector2 textSize = _font.MeasureString(pauseText);
             Vector2 textPosition = new Vector2((WIDTH - textSize.X) / 2, (HEIGHT - textSize.Y) / 2);
-            spriteBatch.DrawString(_font, pauseText, textPosition, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.01f);
+            spriteBatch.DrawString(_font, pauseText, textPosition, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
 
 
 
